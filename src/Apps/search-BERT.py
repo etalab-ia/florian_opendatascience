149dffbdd3ae2faa3ea4247715e26b42afef58a7
Pavel Soriano <pavel.soriano@data.gouv.fr>
2020-09-07 15:35:22 +0200

    started modularizing dash app

--- src/Apps/search-BERT.py
+++ src/Apps/search-BERT.py
@@ -1,5 +1,5 @@
 ######### IMPORTS #############
-#-- DASH-- #
+# -- DASH-- #
 import dash
 import dash_dangerously_set_inner_html
 import dash_bootstrap_components as dbc
@@ -9,70 +9,86 @@ from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 
 import requests
-#-- MISC--#
+# -- MISC--#
 import numpy as np
 import pandas as pd
-import scipy
-from sklearn.manifold import TSNE
 import warnings
+
+from src.Scripts.opendatascience_module import id2vec, neighbours, Search
+
 warnings.filterwarnings('ignore')
 
-#--MODEL--#
+# --MODEL--#
 import torch
 from sentence_transformers import SentenceTransformer, models
-model = SentenceTransformer('distiluse-base-multilingual-cased')
 
-#--LOADS--# (but a separate function should be able to recommpute these from the original csv)
-#data=pd.read_csv('../../data/catalogue_locs_dropped.csv',sep=',', encoding='latin-1')
-id_table_new=list(np.load('../../data/test/id_table.npy', allow_pickle=True))
-embeddings=np.load('../../data/test/embeddings.npy', allow_pickle=True)
+
+# model = SentenceTransformer('distiluse-base-multilingual-cased')
+# --LOADS--# (but a separate function should be able to recommpute these from the original csv)
+# data=pd.read_csv('../../data/catalogue_locs_dropped.csv',sep=',', encoding='latin-1')
+# id_table_new = list(np.load('../../data/test/id_table.npy', allow_pickle=True))
+# embeddings = np.load('../../data/test/embeddings.npy', allow_pickle=True)
+
+
 ####### SEARCH FUNCTIONS ##########
-def query2vec(search_query):
-    return(model.encode([search_query]))
+# def query2vec(search_query):
+#     return(model.encode([search_query]))
+#
+# def get_id(idx):
+#     dataset_id = id_table_new[idx]
+#     return (dataset_id)
 
-def get_id(idx):
-    dataset_id=id_table_new[idx]
-    return(dataset_id)
 
-def get_idx(ids):
-    dataset_idx=id_table_new.index(ids)
-    return(dataset_idx)
+# def get_idx(ids):
+#     dataset_idx = id_table_new.index(ids)
+#     return (dataset_idx)
 
-def id2vec(ids):
-    return(list(embeddings[get_idx(ids)]))
 
-def neighbours(vector, n):
-    n_ids=[]
-    score=[]
-    distances = scipy.spatial.distance.cdist(vector, embeddings, "cosine")[0]
-    results = zip(range(len(distances)), distances)
-    results = sorted(results, key=lambda x: x[1])
-    for idx, distance in results[0:n]:
-        n_ids.append(get_id(idx))
-        score.append(1-distance)
-    return(n_ids, score, vector)
+# def id2vec(ids):
+#     return (list(embeddings[get_idx(ids, id_table_new=id_table_new)]))
 
-def find_vectors(vector, n_ids):
-    vectors=[]
-    for ids in n_ids:
-        vectors.append(id2vec(ids))
-    vectors.append(vector[0])
-    tsne_vec = TSNE(n_components=2).fit_transform(vectors)
-    return(tsne_vec)
 
-def Search(search_query, n):
-    n_ids, score, vector=neighbours(query2vec(search_query), n)
-    tsne_vec=find_vectors(vector, n_ids)
-    #print(n_ids, score)
-    return(n_ids, score, tsne_vec)
+# def neighbours(vector, n, embeddings, id_table_new):
+#     n_ids = []
+#     score = []
+#     distances = scipy.spatial.distance.cdist(vector, embeddings, "cosine")[0]
+#     results = zip(range(len(distances)), distances)
+#     results = sorted(results, key=lambda x: x[1])
+#
+#     for idx, distance in results[0:n]:
+#         n_ids.append(get_id(idx, id_table_new=id_table_new))
+#         score.append(1 - distance)
+#     return (n_ids, score, vector)
+
+#
+# def find_vectors(vector, n_ids):
+#     vectors = []
+#     for ids in n_ids:
+#         vectors.append(id2vec(ids, embeddings=embeddings, id_table_new=id_table_new))
+#     vectors.append(vector[0])
+#     tsne_vec = TSNE(n_components=2).fit_transform(vectors)
+#     return (tsne_vec)
+
+#
+# def Search(search_query, n):
+#
+#
+#     n_ids, score, vector = neighbours(query2vec(search_query, model=model), n)
+#     tsne_vec = tsne_vectors(vector, n_ids,
+#                             embeddings=embeddings, id_table_new=id_table_new)
+#     # print(n_ids, score)
+#     return (n_ids, score, tsne_vec)
+
 
 def Similarity(ids, n):
-    n_ids, score=neighbours(id2vec(ids), n)
-    return(n_ids, score)
+    n_ids, score = neighbours(id2vec(ids), n)
+    return (n_ids, score)
+
+
 #######
 
 
-DATAGOUV='https://static.data.gouv.fr/_themes/gouvfr/img/logo-social.png?_=2.1.4'
+DATAGOUV = 'https://static.data.gouv.fr/_themes/gouvfr/img/logo-social.png?_=2.1.4'
 
 search_bar = dbc.Row(
     [
@@ -94,7 +110,7 @@ navbar = dbc.Navbar(
             dbc.Row(
                 [
                     dbc.Col(html.Img(src=DATAGOUV, height="40px")),
-                    dbc.Col(dbc.NavbarBrand("Rechercher un jeu de donnée", style={'color':'White'}, className="ml-2")),
+                    dbc.Col(dbc.NavbarBrand("Rechercher un jeu de donnée", style={'color': 'White'}, className="ml-2")),
                 ],
                 align="center",
                 no_gutters=True,
@@ -107,14 +123,17 @@ navbar = dbc.Navbar(
     color="MediumBlue",
 )
 
+
 #####################"
 def dataset(ids):
     res = requests.get(f'https://www.data.gouv.fr/api/1/datasets/{ids}')
     return res.json()
-    #idx=get_idx(ids)
-    #print(idx)
-    #df=data.loc[idx]
-    #return(df)
+    # idx=get_idx(ids)
+    # print(idx)
+    # df=data.loc[idx]
+    # return(df)
+
+
 def embed(ids, score):
     # html = dash_dangerously_set_inner_html.DangerouslySetInnerHTML(f"""
     #     <div data-udata-dataset="{id}"></div>
@@ -125,25 +144,25 @@ def embed(ids, score):
     output = html.Tr([
         html.Td(html.A(ds['id'], href=f"https://www.data.gouv.fr/fr/datasets/{ids}"), colSpan=1),
         html.Td(html.A(ds['title'], href=f"https://www.data.gouv.fr/fr/datasets/{ids}"), colSpan=1),
-       # html.Td(ds['description'], colSpan=2),
-        #html.Td(ds['pred locs'], colSpan=1),
+        # html.Td(ds['description'], colSpan=2),
+        # html.Td(ds['pred locs'], colSpan=1),
         html.Td(score, colSpan=1),
     ])
 
     return output
+
 
 ############"
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = html.Div(children=[
     dbc.Row(dbc.Col(
-    html.Div( navbar
-             ))),
-    dbc.Row([dbc.Col(dbc.Spinner(html.Div(id='my-output')),width=7),
-    dbc.Col(dcc.Graph(id='graph'), width=5)]),
+        html.Div(navbar
+                 ))),
+    dbc.Row([dbc.Col(dbc.Spinner(html.Div(id='my-output')), width=7),
+             dbc.Col(dcc.Graph(id='graph'), width=5)]),
     html.Div(id='knns', style={'display': 'none'})
 ])
-
 
 
 @app.callback(
@@ -151,52 +170,53 @@ app.layout = html.Div(children=[
     [Input(component_id='my-input', component_property='value')]
 )
 def compute(input_value):
-    knns=Search(input_value, 10)
-    knns=pd.DataFrame(knns).to_json()
+    knns = Search(input_value, 10)
+    knns = pd.DataFrame(knns).to_json()
     return knns
-    
-    
-    
+
+
 @app.callback(
     Output(component_id='my-output', component_property='children'),
     [Input(component_id='knns', component_property='children')]
 )
 def update_output_div(knns):
-    knns=(list(pd.read_json(knns).loc[0])[:-1],list(pd.read_json(knns).loc[1])[:-1]  )
-    results = [ embed(i, j) for i,j in zip(knns[0], knns[1] ) ]
+    knns = (list(pd.read_json(knns).loc[0])[:-1], list(pd.read_json(knns).loc[1])[:-1])
+    results = [embed(i, j) for i, j in zip(knns[0], knns[1])]
     # print(f" 👀 {input_value}")
     # print(f" results: {results}")
-#    results = html.Div([ embed(r) for r in Search(input_value, 10) ])
-#    return 'Output: {}'.format(Search(input_value, 10))
+    #    results = html.Div([ embed(r) for r in Search(input_value, 10) ])
+    #    return 'Output: {}'.format(Search(input_value, 10))
 
     header = [
         html.Thead(html.Tr([
             html.Th("id", colSpan=1),
             html.Th("titre", colSpan=1),
-          #  html.Th("description", colSpan=2),
-         #   html.Th("Localisation", colSpan=1),
+            #  html.Th("description", colSpan=2),
+            #   html.Th("Localisation", colSpan=1),
             html.Th("Distance", colSpan=1),
         ]))
     ]
 
-    return dbc.Table(header + [html.Tbody(results)], bordered=True, hover=True, responsive=True,  striped=True)
+    return dbc.Table(header + [html.Tbody(results)], bordered=True, hover=True, responsive=True, striped=True)
+
 
 @app.callback(Output(component_id='graph', component_property='figure'), [Input('knns', 'children')])
 def update_graph(knns):
-    knns=pd.read_json(knns)
-    xy=np.array(list(knns.loc[2])).transpose()
-    color_list=['blue']*len(xy[0])
-    color_list[-1]='red'
+    knns = pd.read_json(knns)
+    xy = np.array(list(knns.loc[2])).transpose()
+    color_list = ['blue'] * len(xy[0])
+    color_list[-1] = 'red'
     figure = go.Figure(data=go.Scatter(
         x=xy[0],
         y=xy[1],
         mode='markers',
-        text=[ dataset(i)['title'] for i in list(knns.loc[0])[:-1]]+['origin'],
+        text=[dataset(i)['title'] for i in list(knns.loc[0])[:-1]] + ['origin'],
         marker=dict(color=color_list)
     ))
-    
-    #names ds=datasets[knns[0]] ; nmaes=ds['title']
+
+    # names ds=datasets[knns[0]] ; nmaes=ds['title']
     return figure
+
 
 if __name__ == '__main__':
     app.run_server(port=8050, debug=True)
--- src/Scripts/opendatascience_module.py
+++ src/Scripts/opendatascience_module.py
@@ -1,40 +1,45 @@
 import re
 import pandas as pd
 import numpy as np
-#-- MISC--#
+# -- MISC--#
 import scipy.spatial.distance
 import warnings
+import torch
+PATH_LARGE_FILES = "../../data/test"
+MODEL_NAME = "distiluse-base-multilingual-cased"
 ##
 cp_1252_chars = {
     # from http://www.microsoft.com/typography/unicode/1252.htm
-    u"\x80": u"\u20AC", # EURO SIGN
-    u"\x82": u"\u201A", # SINGLE LOW-9 QUOTATION MARK
-    u"\x83": u"\u0192", # LATIN SMALL LETTER F WITH HOOK
-    u"\x84": u"\u201E", # DOUBLE LOW-9 QUOTATION MARK
-    u"\x85": u"\u2026", # HORIZONTAL ELLIPSIS
-    u"\x86": u"\u2020", # DAGGER
-    u"\x87": u"\u2021", # DOUBLE DAGGER
-    u"\x88": u"\u02C6", # MODIFIER LETTER CIRCUMFLEX ACCENT
-    u"\x89": u"\u2030", # PER MILLE SIGN
-    u"\x8A": u"\u0160", # LATIN CAPITAL LETTER S WITH CARON
-    u"\x8B": u"\u2039", # SINGLE LEFT-POINTING ANGLE QUOTATION MARK
-    u"\x8C": u"\u0152", # LATIN CAPITAL LIGATURE OE
-    u"\x8E": u"\u017D", # LATIN CAPITAL LETTER Z WITH CARON
-    u"\x91": u"\u2018", # LEFT SINGLE QUOTATION MARK
-    u"\x92": u"\u2019", # RIGHT SINGLE QUOTATION MARK
-    u"\x93": u"\u201C", # LEFT DOUBLE QUOTATION MARK
-    u"\x94": u"\u201D", # RIGHT DOUBLE QUOTATION MARK
-    u"\x95": u"\u2022", # BULLET
-    u"\x96": u"\u2013", # EN DASH
-    u"\x97": u"\u2014", # EM DASH
-    u"\x98": u"\u02DC", # SMALL TILDE
-    u"\x99": u"\u2122", # TRADE MARK SIGN
-    u"\x9A": u"\u0161", # LATIN SMALL LETTER S WITH CARON
-    u"\x9B": u"\u203A", # SINGLE RIGHT-POINTING ANGLE QUOTATION MARK
-    u"\x9C": u"\u0153", # LATIN SMALL LIGATURE OE
-    u"\x9E": u"\u017E", # LATIN SMALL LETTER Z WITH CARON
-    u"\x9F": u"\u0178", # LATIN CAPITAL LETTER Y WITH DIAERESIS
-} 
+    u"\x80": u"\u20AC",  # EURO SIGN
+    u"\x82": u"\u201A",  # SINGLE LOW-9 QUOTATION MARK
+    u"\x83": u"\u0192",  # LATIN SMALL LETTER F WITH HOOK
+    u"\x84": u"\u201E",  # DOUBLE LOW-9 QUOTATION MARK
+    u"\x85": u"\u2026",  # HORIZONTAL ELLIPSIS
+    u"\x86": u"\u2020",  # DAGGER
+    u"\x87": u"\u2021",  # DOUBLE DAGGER
+    u"\x88": u"\u02C6",  # MODIFIER LETTER CIRCUMFLEX ACCENT
+    u"\x89": u"\u2030",  # PER MILLE SIGN
+    u"\x8A": u"\u0160",  # LATIN CAPITAL LETTER S WITH CARON
+    u"\x8B": u"\u2039",  # SINGLE LEFT-POINTING ANGLE QUOTATION MARK
+    u"\x8C": u"\u0152",  # LATIN CAPITAL LIGATURE OE
+    u"\x8E": u"\u017D",  # LATIN CAPITAL LETTER Z WITH CARON
+    u"\x91": u"\u2018",  # LEFT SINGLE QUOTATION MARK
+    u"\x92": u"\u2019",  # RIGHT SINGLE QUOTATION MARK
+    u"\x93": u"\u201C",  # LEFT DOUBLE QUOTATION MARK
+    u"\x94": u"\u201D",  # RIGHT DOUBLE QUOTATION MARK
+    u"\x95": u"\u2022",  # BULLET
+    u"\x96": u"\u2013",  # EN DASH
+    u"\x97": u"\u2014",  # EM DASH
+    u"\x98": u"\u02DC",  # SMALL TILDE
+    u"\x99": u"\u2122",  # TRADE MARK SIGN
+    u"\x9A": u"\u0161",  # LATIN SMALL LETTER S WITH CARON
+    u"\x9B": u"\u203A",  # SINGLE RIGHT-POINTING ANGLE QUOTATION MARK
+    u"\x9C": u"\u0153",  # LATIN SMALL LIGATURE OE
+    u"\x9E": u"\u017E",  # LATIN SMALL LETTER Z WITH CARON
+    u"\x9F": u"\u0178",  # LATIN CAPITAL LETTER Y WITH DIAERESIS
+}
+
+
 def fix_1252_codes(text):
     """
     Replace non-standard Microsoft character codes from the Windows-1252 character set in a unicode string with proper unicode codes.
@@ -44,10 +49,12 @@ def fix_1252_codes(text):
         def fixup(m):
             s = m.group(0)
             return cp_1252_chars.get(s, s)
+
         if isinstance(text, type("")):
             text = str(text)
         text = re.sub(u"[\x80-\x9f]", fixup, text)
-    return text 
+    return text
+
 
 #
 
@@ -63,25 +70,27 @@ def process_file(file_dir):
     dfx (pandas dataframe of str) :Aggregated text
     id_table (python list of ints): list with the ids in the order of the dfx
     """
-    df=pd.read_csv(file_dir, sep=';',error_bad_lines=False, encoding='latin-1') #le fichier original
-    id_table=np.array(list(df['id']), dtype=object)
-    df=df.astype(str)
-    dfx=df[['title', 'organization', 'description','tags']].agg('. '.join, axis=1)
+    df = pd.read_csv(file_dir, sep=';', error_bad_lines=False, encoding='latin-1')  # le fichier original
+    id_table = np.array(list(df['id']), dtype=object)
+    df = df.astype(str)
+    dfx = df[['title', 'organization', 'description', 'tags']].agg('. '.join, axis=1)
     return dfx, id_table
-    
+
+
 def filter_df(dff, table, new_location):
-    too_short=[]
-    
+    too_short = []
+
     for i in range(len(dff)):
-        if len(dff[i])<45:
+        if len(dff[i]) < 45:
             too_short.append(i)
-    
+
     dff.drop(dff.index[too_short], inplace=True)
-    table=np.delete(table, too_short, axis=0)
-    np.save(new_location+'/id_table.npy', table, allow_pickle=True, fix_imports=True)
-    
+    table = np.delete(table, too_short, axis=0)
+    np.save(new_location + '/id_table.npy', table, allow_pickle=True, fix_imports=True)
+
     return dff, table
-    
+
+
 def super_cleaning(dfx):
     """dataframe is cleaned of the unicode errors, separators, spaces extensions links etc..
     objective is to keep clean text.
@@ -93,25 +102,25 @@ def super_cleaning(dfx):
     dfx (pandas dataframe of str) :the same df but the text inside is 'cleaned'
    """
     for i in range(len(dfx)):
-        dfx[i]=fix_1252_codes(dfx[i])
-        dfx[i]=re.sub(r'\S*.(fr|org|com|map|jpeg|jpg|zip|png)\b','', dfx[i])
-        dfx[i]=re.sub(r"""
+        dfx[i] = fix_1252_codes(dfx[i])
+        dfx[i] = re.sub(r'\S*.(fr|org|com|map|jpeg|jpg|zip|png)\b', '', dfx[i])
+        dfx[i] = re.sub(r"""
                [,;@#/\\?*[\]\r\n\<\>\_\{\}\»\«\\\(\)!&$]+  # Accept one or more copies of punctuation
                \ *           # plus zero or more copies of a space,
                """,
-               " ",          # and replace it with a single space
-               dfx[i], flags=re.VERBOSE)
-        
-        dfx[i]=re.sub(r'\\x\w+',' ', dfx[i])
-        dfx[i]=re.sub(r'\bx\w+', '', dfx[i])
-        dfx[i]=re.sub(r'\S+\d+\w+', " ", dfx[i])
-        dfx[i]=re.sub(r'(?:^| )\w(?:$| )',' ',dfx[i])
-        dfx[i]=re.sub(' +', ' ', dfx[i])
-    return(dfx)
+                        " ",  # and replace it with a single space
+                        dfx[i], flags=re.VERBOSE)
+
+        dfx[i] = re.sub(r'\\x\w+', ' ', dfx[i])
+        dfx[i] = re.sub(r'\bx\w+', '', dfx[i])
+        dfx[i] = re.sub(r'\S+\d+\w+', " ", dfx[i])
+        dfx[i] = re.sub(r'(?:^| )\w(?:$| )', ' ', dfx[i])
+        dfx[i] = re.sub(' +', ' ', dfx[i])
+    return (dfx)
 
 
-#dataset_in=np.load('../../data/cleaned2.npy', allow_pickle=True) en gros
-#path_out='../../data/multilingual_embeddings.npy'
+# dataset_in=np.load('../../data/cleaned2.npy', allow_pickle=True) en gros
+# path_out='../../data/multilingual_embeddings.npy'
 def embedder(dataset_in, path_out, save):
     import torch
     from sentence_transformers import SentenceTransformer, models
@@ -125,11 +134,12 @@ def embedder(dataset_in, path_out, save):
     embeddings (npy of floats) : The 512 embedding of each dataset as a "big sentence"
     """
     model = SentenceTransformer('distiluse-base-multilingual-cased')
-    dataset_in=dataset_in.to_numpy()
-    embeddings= model.encode(dataset_in, batch_size=8, show_progress_bar=True)
+    dataset_in = dataset_in.to_numpy()
+    embeddings = model.encode(dataset_in, batch_size=8, show_progress_bar=True)
     if save:
-        np.save(path_out+'/embeddings.npy', embeddings, allow_pickle=True)
-    return(embeddings)
+        np.save(path_out + '/embeddings.npy', embeddings, allow_pickle=True)
+    return (embeddings)
+
 
 def save_to(df_clean, new_location):
     """
@@ -142,11 +152,12 @@ def save_to(df_clean, new_location):
     Returns:
     none 
     """
-    df_clean.to_pickle(new_location+'/df_clean.pkl')
-    df_clean_np=df_clean.to_numpy(dtype=object)
-    np.save(new_location+'/df_clean.npy', df_clean_np, allow_pickle=True, fix_imports=True)
+    df_clean.to_pickle(new_location + '/df_clean.pkl')
+    df_clean_np = df_clean.to_numpy(dtype=object)
+    np.save(new_location + '/df_clean.npy', df_clean_np, allow_pickle=True, fix_imports=True)
 
-def clean(file_dir, new_location, df_save_opt ):
+
+def clean(file_dir, new_location, df_save_opt):
     """
     Main function
     Produces the embeddings from the given CSV file location
@@ -161,14 +172,15 @@ def clean(file_dir, new_location, df_save_opt ):
     embeddings (npy of floats) : The 512 embedding of each dataset as a "big sentence"
     id_table (python list of ints): list with the ids in the order of the dfx
     """
-    processed, table=process_file(file_dir)
-    df_clean=super_cleaning(processed)
-    df_clean_filtered, id_table=filter_df(df_clean, table, new_location)
-    embeddings=embedder(df_clean_filtered, new_location, True) 
-                          
+    processed, table = process_file(file_dir)
+    df_clean = super_cleaning(processed)
+    df_clean_filtered, id_table = filter_df(df_clean, table, new_location)
+    embeddings = embedder(df_clean_filtered, new_location, True)
+
     if df_save_opt:
         save_to(df_clean, new_location)
-    return embeddings, id_table 
+    return embeddings, id_table
+
 
 def get_large_files(path):
     """
@@ -181,9 +193,11 @@ def get_large_files(path):
     embeddings (npy of floats) : The 512 embedding of each dataset as a "big sentence"
     id_table (python list of ints): list with the ids in the order of the dfx
     """
-    id_table_new=list(np.load(path+'/id_table.npy', allow_pickle=True))
-    embeddings=np.load(path+'/embeddings.npy', allow_pickle=True)
-    return(embeddings, id_table_new)
+    id_table_new = list(np.load(path + '/id_table.npy', allow_pickle=True))
+    embeddings = np.load(path + '/embeddings.npy', allow_pickle=True)
+    return (embeddings, id_table_new)
+
+
 ####### SEARCH FUNCTIONS ##########
 def query2vec(search_query):
     """
@@ -195,12 +209,10 @@ def query2vec(search_query):
     Returns:
     (numpy array of floats) encoded search query in the 512 floats of the output model embedding
     """
-    import torch
-    from sentence_transformers import SentenceTransformer, models
-    model = SentenceTransformer('distiluse-base-multilingual-cased')
-    return(model.encode([search_query]))
+    return (model.encode([search_query]))
 
-def get_id(idx):
+
+def get_id(idx, id_table_new):
     """
     Gets the ID given an index in the table of datasets  
 
@@ -210,8 +222,9 @@ def get_id(idx):
     Returns:
     dataset_id (str): The unique dataset identitfier from data gouv
     """
-    dataset_id=id_table_new[idx]
-    return(dataset_id)
+    dataset_id = id_table_new[idx]
+    return (dataset_id)
+
 
 def get_idx(ids):
     """
@@ -223,8 +236,9 @@ def get_idx(ids):
     Returns:
     idx (int): Datasets idx in the order of the table
     """
-    dataset_idx=id_table_new.index(ids)
-    return(dataset_idx)
+    dataset_idx = id_table_new.index(ids)
+    return (dataset_idx)
+
 
 def id2vec(ids):
     """
@@ -236,7 +250,8 @@ def id2vec(ids):
     Returns:
     (list of numpy array of floats) : the datasets embeddings
     """
-    return(list(embeddings[get_idx(ids)]))
+    return (list(embeddings[get_idx(ids)]))
+
 
 def neighbours(vector, n):
     """
@@ -253,15 +268,18 @@ def neighbours(vector, n):
     vector (np array): The vector of the search query or dataset to look for similarity needed for another function
     no processing done from input vector
     """
-    n_ids=[]
-    score=[]
+
+
+    n_ids = []
+    score = []
     distances = scipy.spatial.distance.cdist(vector, embeddings, "cosine")[0]
     results = zip(range(len(distances)), distances)
     results = sorted(results, key=lambda x: x[1])
     for idx, distance in results[0:n]:
-        n_ids.append(get_id(idx))
-        score.append(1-distance)
-    return(n_ids, score, vector)
+        n_ids.append(get_id(idx, id_table_new=id_table_new))
+        score.append(1 - distance)
+    return (n_ids, score, vector)
+
 
 def tsne_vectors(vector, n_ids):
     """
@@ -275,12 +293,13 @@ def tsne_vectors(vector, n_ids):
     tsne_vec (array of arrays of vectors 2d (from 512)): TSNE(2d) of the input vectors(512d)
     """
     from sklearn.manifold import TSNE
-    vectors=[]
+    vectors = []
     for ids in n_ids:
         vectors.append(id2vec(ids))
     vectors.append(vector[0])
     tsne_vec = TSNE(n_components=2).fit_transform(vectors)
-    return(tsne_vec)
+    return (tsne_vec)
+
 
 def request_api(ids):
     """
@@ -295,7 +314,8 @@ def request_api(ids):
     import requests
     res = requests.get(f'https://www.data.gouv.fr/api/1/datasets/{ids}')
     return res.json()
-    
+
+
 def request_results(n_ids):
     """
     Gives the titles of the ids foud by search and requested
@@ -306,31 +326,41 @@ def request_results(n_ids):
     Returns:
     rsults (list of str): all the titles of the search results
     """
-    results = [ request_api(i)['title'] for i in n_ids ]
-    return(results) 
-    
+    results = [request_api(i)['title'] for i in n_ids]
+    return (results)
+
 
 def Search(search_query, n):
     """
-    Does the Search (just the concatenation of previous functions) 
-    
+    Does the Search (just the concatenation of previous functions)
+
     Keyword argument:
     search_query (str): Search query of the user
     n (int): The number of neighbours to look for
-    
-    
+
+
     Returns:
     n_ids (list of str): all the ids (data gouv) of the n nearest neighbours
     score (list of float): the corresponding scores decreasing order, of the distance to the dataset
     the higher the score the better: 1-dista
     tsne_vec (array of arrays of vectors 2d (from 512)): TSNE(2d) of the input vectors(512d)
     """
-    global embeddings
-    global id_table_new
-    embeddings, id_table_new=get_large_files('../../data/test')
-    n_ids, score, vector=neighbours(query2vec(search_query), n)
-    tsne_vec=tsne_vectors(vector, n_ids)
-    return(n_ids, score, tsne_vec)
+
+    if "model" not in globals():
+        global model
+        from sentence_transformers import SentenceTransformer
+        model = SentenceTransformer(MODEL_NAME)
+
+    if "embeddings" not in globals() or "id_table_new" not in globals():
+        global embeddings
+        global id_table_new
+        embeddings, id_table_new = get_large_files(PATH_LARGE_FILES)
+
+
+    n_ids, score, vector = neighbours(query2vec(search_query), n)
+    tsne_vec = tsne_vectors(vector, n_ids)
+    return (n_ids, score, tsne_vec)
+
 
 def Similarity(ids, n):
     """
@@ -345,5 +375,5 @@ def Similarity(ids, n):
     n_ids (list of str): all the ids (data gouv) of the n most similar datasets
     score (list of float): the corresponding scores decreasing order, of the distance to the dataset
     """
-    n_ids, score=neighbours(id2vec(ids), n)
-    return(n_ids, score)
+    n_ids, score, _ = neighbours(id2vec(ids), n)
+    return (n_ids, score)
